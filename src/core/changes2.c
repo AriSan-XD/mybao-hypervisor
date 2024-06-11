04b0b3706f47ae6eaad5036ee7347c1c1a8dea72
arisan <helloarisan@foxmail.com>
2024-06-11 13:13:36 +0800

    hypercall no context switch

--- src/core/changes2.c
+++ src/core/changes2.c
@@ -8,6 +8,12 @@ long int changeS2(void)
     
     // printk("hello from changes2\n");
 
+    // uint64_t vttbr = 0;
+    
+    // vttbr = sysreg_vttbr_el2_read();
+
+    // sysreg_vttbr_el2_write(vttbr);
+
     return -HC_E_SUCCESS;
 
 }
